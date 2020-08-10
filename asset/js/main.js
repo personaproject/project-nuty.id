d3089013c5295ea02a900bca6b7c54da2e9b62a5
me.msyariefh <me.msyariefh@gmail.com>
2020-08-10 10:10:28 +0700

    add dismissible and noMove

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,30 +8,46 @@ document.addEventListener('DOMContentLoaded', function() {
         indicators : true,
     });
     var modal = document.querySelectorAll(".modal");
-    var initmodal = M.Modal.init(modal, {});
+    var initmodal = M.Modal.init(modal, {
+        dismissible:false,
+        startingTop: "unset",
+        endingTop: "unset",
+    });
   });
+  function noMove(){
+    $.fn.pagepiling.setMouseWheelScrolling(false);
+  }
+  function allowMove(){
+    $.fn.pagepiling.setMouseWheelScrolling(true);
+  }
 function arise(){
     d = document.querySelectorAll(".dalam");
     e = document.querySelectorAll(".bg-change");
+    e.forEach(element => {
+        element.style.animation = "slideInUp";
+        element.style.animationDuration = "0.5s";
+        element.style.backgroundColor = "transparent";
+    });
     d.forEach(element => {
         element.style.display = "block";
-        element.style.transition = "0.5 ease in"
+        // element.style.animation = "slideOutDown";
+        element.style.animationDuration = "0.5s"
+        // element.style.animationDelay = "0.5s";
     });
-    e.forEach(element => {
-        element.style.backgroundColor = "transparent";
-        element.style.transition = "0.5 ease in"
-    });
+
 }
 function disappear(){
     d = document.querySelectorAll(".dalam");
     e = document.querySelectorAll(".bg-change");
     d.forEach(element => {
         element.style.display = "none";
-        element.style.transition = "0.5 ease out"
+        element.style.animation = "slideInUp";
+        element.style.animationDuration = "0.5s";
     });
     e.forEach(element => {
         element.style.backgroundColor = "rgba(0, 0, 0, .6)";
-        element.style.transition = "0.5 ease out"
+        // element.style.animation = "slideOutUp";
+        element.style.transition ="0.5s ease-in-out"
     });
 }
 window.onscroll = function(){scrollFunction()};
